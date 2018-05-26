f7d4e5c9d41d3349b84a6c52b80decdfd2999267
r4tt1esnake <r4tt1esnake@users.noreply.github.com>
2018-05-25 21:08:10 -0700

    Initial Commit

---
+++ ToughBeingTeen/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ToughBeingTeen
+//
+//  Created by Alex Yu Chen Ni on 2018-05-25.
+//  Copyright © 2018 Alex Yu Chen Ni. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
