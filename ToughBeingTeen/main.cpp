57bfdabfd6f0c1b4bb24ce98f4c2a8bb3b7aa686
r4tt1esnake <r4tt1esnake@users.noreply.github.com>
2018-05-29 00:23:55 -0700

    Added

--- ToughBeingTeen/main.cpp
+++ ToughBeingTeen/main.cpp
@@ -9,7 +9,34 @@
 #include <iostream>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    /* http://www.cemc.uwaterloo.ca/contests/computing/2006/stage1/juniorEn.pdf */
+    
+    bool taskList[7][7] = {false};
+    taskList[0][6] = true;
+    taskList[0][3] = true;
+    taskList[1][0] = true;
+    taskList[2][3] = true;
+    taskList[2][4] = true;
+    
+    while(true) {
+        
+        int a, b;
+        std::cin >> a;
+        std::cin >> b;
+        
+        if(a == 0 && b == 0) {
+            break;
+        }
+        
+        a--;
+        b--;
+        
+        taskList[a][b] = true;
+        
+    }
+    
+    
+    
     return 0;
 }
